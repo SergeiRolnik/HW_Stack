82f84c18f067f86ecddf382efb9caa0cde2b97c0
Сергей Рольник <forgedwheels@gmail.com>
2022-04-27 17:58:19 +0300

    HW complete

---
+++ main.py
@@ -0,0 +1,43 @@
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def isEmpty(self):
+        return len(self.stack) == 0
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if self.isEmpty():
+            return None
+        else:
+            return self.stack.pop()
+
+    def peek(self):
+        return self.stack[-1]
+
+    def size(self):
+        return len(self.stack)
+
+open_brackets = ["[", "{", "("]
+close_brackets = ["]", "}", ")"]
+
+def check_balance(str):
+    s = Stack()
+    for bracket in str:
+        if bracket in open_brackets:
+            s.push(bracket)
+        elif bracket in close_brackets:
+            if not s.isEmpty() and open_brackets[close_brackets.index(bracket)] == s.peek():
+                s.pop()
+            else:
+                return 'Unbalanced'
+    print(s.stack)
+    if s.isEmpty():
+        return 'Balanced'
+
+strings = ['(((([{}]))))', '[([])((([[[]]])))]{()}', '{{[()]}}', '}{}', '{{[(])]}}', '[[{())}]', '(({{']
+for str in strings[-1]:
+    print(str, check_balance(str))
